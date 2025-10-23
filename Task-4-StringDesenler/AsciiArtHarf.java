7f3c8fbf3d90d4c9303e95ecfb4fafe38c1b202a
Selamelali <250541619@firat.edu.tr>
2025-10-23 23:35:08 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,9 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("  A   ");
+        System.out.println(" A A  ");
+        System.out.println("AAAAA ");
+        System.out.println("A   A ");
+        System.out.println("A   A ");
+    }
+}
