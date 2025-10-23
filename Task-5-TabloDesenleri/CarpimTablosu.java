d82a6b40017262c0df95847152218dac5d5f9849
Selamelali <250541619@firat.edu.tr>
2025-10-23 23:38:33 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,9 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System.out.println("2'in Çarpanları");
+        System.out.println("-----------------");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("2 x %d = %d\n", i, 2 * i);
+        }
+    }
+}
