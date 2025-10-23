8e9c67ed146dea79a66f4ef2edd214afe6136fa6
Selamelali <250541619@firat.edu.tr>
2025-10-23 23:15:21 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BesKereTekrar {
+    public static void main(String[] args) {
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+    }
+}
