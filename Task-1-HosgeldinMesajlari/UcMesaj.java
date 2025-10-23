39ba382028b3c4dfe20ec7eb1b5a8c99244ce9ec
Selamelali <250541619@firat.edu.tr>
2025-10-23 22:30:52 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Programlama çok eğlenceli!");
+        System.out.println("Başarı için pratik şart!");
+    }
+}
