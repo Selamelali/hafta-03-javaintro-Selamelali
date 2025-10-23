8fc3d1e660907fa53b98778b6e8b4a48b160c6a4
Selamelali <250541619@firat.edu.tr>
2025-10-23 23:33:22 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ahmet";
+        String soyad = "Yılmaz";
+        String ogrNo = "220301010";
+        String bolum = "Yazılım Müh.";
+        
+        System.out.println("====================");
+        System.out.println("ÖĞRENCİ BİLGİLERİ");
+        System.out.println("====================");
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        System.out.println("No: " + ogrNo);
+        System.out.println("Bölüm: " + bolum);
+        System.out.println("====================");
+    }
+}
